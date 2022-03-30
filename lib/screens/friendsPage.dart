a59028bb9145e7f59cd97889e5e878dc53c92cab
ColeTown <c4townsend@gmail.com>
2022-03-29 21:00:58 -0500

    Added basic placeholder UI for bottom navigation bar and friend list and chats

---
+++ lib/screens/friendsPage.dart
@@ -0,0 +1,34 @@
+import 'package:blind_app/screens/chatPage.dart';
+import 'package:flutter/material.dart';
+
+class FriendsPage extends StatelessWidget{
+  const FriendsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ChatPage(),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey.shade600,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w600),
+        unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.w600),
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people_outline),
+            label: 'Friends',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
