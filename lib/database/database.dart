b2e5beb55fa395d6ffae08d299a2324a9448a5e3
ColeTown <c4townsend@gmail.com>
2022-04-04 13:06:15 -0500

    made MongoDatabase class that will eventually handle all possible queries to the database. Currently only handles connection initialization

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -2,10 +2,12 @@ import 'package:mongo_dart/mongo_dart.dart';
 
 class MongoDatabase {
 
-  static connect() async {
-    db = await Db.create(MONGO_CONN_URL);
+  var userCollection;
+
+  connect() async {
+    var db = await Db.create('mongodb+srv://jmarks:Nzsp_C7pagvgm34@cluster0.8kwix.mongodb.net/BlindAppDB?retryWrites=true&w=majority');
     await db.open();
-    userCollection = db.collection(USER_COLLECTION);
+    userCollection = db.collection('Users');
   }
 
 }
