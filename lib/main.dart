6a6c3c24c67ff7ee859611d5ce769ed42b0b0ced
ColeTown <c4townsend@gmail.com>
2022-04-05 00:15:12 -0500

    Got chat page to populate correctly from database

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:blind_app/screens/friendsPage.dart';
 import 'package:blind_app/screens/navBar.dart';
 import 'package:flutter/material.dart';
 
+import 'database/database.dart';
+var db = MongoDatabase();
+
 void main() {
+  db.connect();
   runApp(MyApp());
 }
+
+
 
 class MyApp extends StatelessWidget {
   @override
--- lib/screens/chatDetailPage.dart
+++ lib/screens/chatDetailPage.dart
@@ -1,30 +1,44 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 import '../database/database.dart';
+import '../main.dart';
 import '../models/chatMessageModel.dart';
 
-
-class ChatDetailPage extends StatefulWidget{
+class ChatDetailPage extends StatefulWidget {
   @override
   _ChatDetailPageState createState() => _ChatDetailPageState();
 }
 
 class _ChatDetailPageState extends State<ChatDetailPage> {
-
   String localUserId = 'testing sender';
   String friendUserId = 'testing receiver';
-  var db = MongoDatabase();
+  // var db = MongoDatabase();
   final textController = TextEditingController();
 
-   Future<List> getMessages() async {
+  Future<List> getMessages() async {
     List<ChatMessage> messages = [];
-    db.connect();
+    // await db.connect();
     List fromUser = await db.getMessages(localUserId, friendUserId);
     List toUser = await db.getMessages(friendUserId, localUserId);
-    fromUser.forEach((message) => messages.add(ChatMessage(messageContent: message.text, messageType: 'sender', timeSent: message.time_sent)));
-    toUser.forEach((message) => messages.add(ChatMessage(messageContent: message.text, messageType: 'receiver', timeSent: message.time_sent)));
-    messages.sort((a, b) => a.timeSent.compareTo(b.timeSent));
-    print(messages);
+    for (var message in fromUser) {
+      try {
+        messages.add(ChatMessage(
+            messageContent: message['text'],
+            messageType: 'sender',
+            timeSent: message['time_sent']));
+      } catch (e) {
+        print(e);
+      }
+    }
+    for (var message in toUser) {
+      messages.add(ChatMessage(
+          messageContent: message['text'],
+          messageType: 'receiver',
+          timeSent: message['time_sent']));
+    }
+    messages.sort((a, b) => b.timeSent.compareTo(a.timeSent));
     return messages;
   }
 
@@ -37,138 +51,166 @@ class _ChatDetailPageState extends State<ChatDetailPage> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: getMessages(),
-      builder: (context, AsyncSnapshot<List> snapshot) {
-        if (snapshot.hasData) {
-          return Scaffold(
-            appBar: AppBar(
-              elevation: 0,
-              automaticallyImplyLeading: false,
-              backgroundColor: Colors.white,
-              flexibleSpace: SafeArea(
-                child: Container(
-                  padding: EdgeInsets.only(right: 16),
-                  child: Row(
-                    children: <Widget>[
-                      IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: const Icon(Icons.arrow_back, color: Colors.black,),
-                      ),
-                      SizedBox(width: 2,),
-                      CircleAvatar(
-                        backgroundImage: NetworkImage(
-                            "https://randomuser.me/api/portraits/men/5.jpg"),
-                        maxRadius: 20,
-                      ),
-                      SizedBox(width: 12,),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text("Kriss Benwat", style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.w600),),
-                          ],
-                        ),
-                      ),
-                      const Icon(Icons.settings, color: Colors.black54,),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            body: Stack(
-              children: <Widget>[
-                ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  shrinkWrap: true,
-                  padding: const EdgeInsets.only(top: 10, bottom: 10),
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemBuilder: (context, index) {
-                    return Container(
-                      padding: const EdgeInsets.only(
-                          left: 14, right: 14, top: 10, bottom: 10),
-                      child: Align(
-                        alignment: (snapshot.data![index].messageType ==
-                            "receiver"
-                            ? Alignment.topLeft
-                            : Alignment.topRight),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: (snapshot.data![index].messageType ==
-                                "receiver"
-                                ? Colors.grey.shade200
-                                : Colors.blue[200]),
-                          ),
-                          padding: const EdgeInsets.all(16),
-                          child: Text(snapshot.data![index].messageContent,
-                            style: const TextStyle(fontSize: 15),),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-                Align(
-                  alignment: Alignment.bottomLeft,
+        future: getMessages(),
+        builder: (context, AsyncSnapshot<List> snapshot) {
+          if (snapshot.hasData) {
+            return Scaffold(
+              appBar: AppBar(
+                elevation: 0,
+                automaticallyImplyLeading: false,
+                backgroundColor: Colors.white,
+                flexibleSpace: SafeArea(
                   child: Container(
-                    padding: const EdgeInsets.only(left: 10, bottom: 10, top: 10),
-                    height: 60,
-                    width: double.infinity,
-                    color: Colors.white,
+                    padding: EdgeInsets.only(right: 16),
                     child: Row(
                       children: <Widget>[
-                        GestureDetector(
-                          onTap: () {},
-                          child: Container(
-                            height: 30,
-                            width: 30,
-                            decoration: BoxDecoration(
-                              color: Colors.lightBlue,
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                            child: const Icon(
-                              Icons.add, color: Colors.white, size: 20,),
-                          ),
-                        ),
-                        const SizedBox(width: 15,),
-                        Expanded(
-                          child: TextField(
-                            controller: textController,
-                            decoration: const InputDecoration(
-                                hintText: "Write message...",
-                                hintStyle: TextStyle(color: Colors.black54),
-                                border: InputBorder.none
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 15,),
-                        FloatingActionButton(
+                        IconButton(
                           onPressed: () {
-                            db.insertMessage(
-                                textController.text, localUserId,
-                                friendUserId,
-                                DateTime.now());
+                            Navigator.pop(context);
                           },
-                          child: const Icon(Icons.send, color: Colors.white,
-                            size: 18,),
-                          backgroundColor: Colors.blue,
-                          elevation: 0,
+                          icon: const Icon(
+                            Icons.arrow_back,
+                            color: Colors.black,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 2,
+                        ),
+                        CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              "https://randomuser.me/api/portraits/men/5.jpg"),
+                          maxRadius: 20,
+                        ),
+                        SizedBox(
+                          width: 12,
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                "Kriss Benwat",
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.w600),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const Icon(
+                          Icons.settings,
+                          color: Colors.black54,
                         ),
                       ],
-
                     ),
                   ),
                 ),
-              ],
-            ),
-          );
-        } else {
-          return Container();
-        }
-      }
-    );
+              ),
+              body: Stack(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      shrinkWrap: true,
+                      reverse: true,
+                      padding: const EdgeInsets.only(top: 10, bottom: 100),
+                      physics: const BouncingScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        return Container(
+                          padding: const EdgeInsets.only(
+                              left: 14, right: 14, top: 10, bottom: 10),
+                          child: Align(
+                            alignment:
+                                (snapshot.data![index].messageType == "receiver"
+                                    ? Alignment.topLeft
+                                    : Alignment.topRight),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: (snapshot.data![index].messageType ==
+                                        "receiver"
+                                    ? Colors.grey.shade200
+                                    : Colors.blue[200]),
+                              ),
+                              padding: const EdgeInsets.all(16),
+                              child: Text(
+                                snapshot.data![index].messageContent,
+                                style: const TextStyle(fontSize: 15),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Container(
+                      padding:
+                          const EdgeInsets.only(left: 10, bottom: 10, top: 10),
+                      height: 60,
+                      width: double.infinity,
+                      color: Colors.white,
+                      child: Row(
+                        children: <Widget>[
+                          GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              height: 30,
+                              width: 30,
+                              decoration: BoxDecoration(
+                                color: Colors.lightBlue,
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              child: const Icon(
+                                Icons.add,
+                                color: Colors.white,
+                                size: 20,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          Expanded(
+                            child: TextField(
+                              controller: textController,
+                              decoration: const InputDecoration(
+                                  hintText: "Write message...",
+                                  hintStyle: TextStyle(color: Colors.black54),
+                                  border: InputBorder.none),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          FloatingActionButton(
+                            onPressed: () {
+                              setState(() {});
+                              var temp = textController.text;
+                              textController.clear();
+                              db.insertMessage(temp, localUserId, friendUserId,
+                                  DateTime.now());
+                            },
+                            child: const Icon(
+                              Icons.send,
+                              color: Colors.white,
+                              size: 18,
+                            ),
+                            backgroundColor: Colors.blue,
+                            elevation: 0,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return Container();
+          }
+        });
   }
 }
