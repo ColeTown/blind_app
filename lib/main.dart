a108370820bc45682226a9c676c5a6d6bb9da42e
Cantrell Picou Jr <cantrellpicoujr@Cantrells-MacBook-Pro.local>
2022-04-05 22:27:11 -0500

    pushed

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:blind_app/screens/login.dart';
 import 'package:flutter/material.dart';
 import 'geolocation/geolocation.dart';
 import 'database/database.dart';
+import 'python/pythonapi.dart';
 
 var db = MongoDatabase();
 var geo = GeoLocation();
+var py = Python();
 
 String localUserId = 'then-dog-1993';
 
@@ -12,10 +14,17 @@ void main() {
   db.connect();
   runApp(MyApp());
   pushCurrentLocationToDB();
+  getResponse(localUserId);
 }
 
 pushCurrentLocationToDB() async {
   await geo.getCurrentLocation();
+}
+
+getResponse(String userId) {
+
+  py.getResponse(userId);
+
 }
 
 class MyApp extends StatelessWidget {
