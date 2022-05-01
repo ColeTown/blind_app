b19efb012c4dd5d73f8d346f718b0f71c8115ffa
AndMcKen <anderson.mckennon@ttu.edu>
2022-04-30 22:15:16 -0500

    Created a test for db.setBio. Deleted un-needed extra tab for edit profile

--- lib/Unit Tests/tests.dart
+++ lib/Unit Tests/tests.dart
@@ -24,22 +24,15 @@ void unitTests() async {
       expect(results.toString(), expected);
   });
 
-
   test('TCCP01: Testing matching algorithm', () async {
 
     String url = "";
-
     var data = "";
-
     String usrName = 'then-dog-1993';
-
     url = 'http://10.0.2.2:5000/api?query=' + usrName;
     data =  await getData(url);
-    
     var decoded = jsonDecode(data);
-
     var expected = '[charming-cat-972, middle-mole-4206]';
-
 
     expect(decoded['output'].toString(), expected);
 
@@ -53,6 +46,22 @@ void unitTests() async {
     var expected = '[true, american-hyena-8431]';
 
     expect(results.toString(), expected);
+  });
+
+  test('TCAM01: Testing changes to user Bio within Database', () async{
+    String testString = 'This is a bio from a TEST';
+    await db.setBio('then-dog-1993', testString);
+
+    var results = await db.getUsers('then-dog-1993');
+    results[0]['pfp'] = "";
+    var expected = '[{_id: ObjectId("62489a46d0ce025858c69c2d"), '
+        'fname: Pierre, lname: Hastings, userid: then-dog-1993, '
+        'email: phastings@guerrillamail.com, pw: asdf1234, '
+        'joindate: 2022-04-02 05:00:00.000Z, bio: $testString, '
+        'pfp: }]';
+
+    expect(results.toString(), expected);
+
   });
 
 
